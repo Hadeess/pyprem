3a041c9161dd9297e0c58fb7b1497f73da050d3b
Hadeess <39347872+Hadeess@users.noreply.github.com>
2021-05-10 11:26:23 +0100

    added La Liga to the DB

--- data.py
+++ data.py
@@ -29,5 +29,34 @@ variables = {
 		"westbrom":"https://www.scorespro.com/soccer/england/teams/west-bromwich-MjQ1NA==/",
 		"fulham":"https://www.scorespro.com/soccer/england/teams/fulham-fc-MjQyMg==/",
 		"burnley":"https://www.scorespro.com/soccer/england/teams/burnley-fc-MTA4MzQ=/"
+	},
+	"laliga": {
+		"_l_name":"https://www.scorespro.com/soccer/spain/laliga/",
+		"_l_name_r":"https://www.scorespro.com/soccer/spain/laliga/results/",
+		"_l_name_f":"https://www.scorespro.com/soccer/spain/laliga/fixtures/",
+		"_l_name_t":"https://www.scorespro.com/soccer/spain/laliga/standings/",
+		"athletico": "https://www.scorespro.com/soccer/spain/teams/atletico-madrid-MTgwNjM=/",
+		"realmadrid": "https://www.scorespro.com/soccer/spain/teams/real-madrid-NDE4MA==/",
+		"barcelona": "https://www.scorespro.com/soccer/spain/teams/fc-barcelona-MTc3NjI=/",
+		"sevilla": "https://www.scorespro.com/soccer/spain/teams/sevilla-MjYyNTM=/",
+		"realsociedad": "https://www.scorespro.com/soccer/spain/teams/real-sociedad-MTgwMzI=/",
+		"villarreal": "https://www.scorespro.com/soccer/spain/teams/villarreal-MTc4NDI=/",
+		"realbetis": "https://www.scorespro.com/soccer/spain/teams/real-betis-MTgwMjI=/",
+		"celtavigo": "https://www.scorespro.com/soccer/spain/teams/celta-vigo-NDE3Mg==/",
+		"athleticclub": "https://www.scorespro.com/soccer/spain/teams/athletic-bilbao-MTgwMTk=/",
+		"granada": "https://www.scorespro.com/soccer/spain/teams/granada-cf-Mzc4MDE=/",
+		"cadiz": "https://www.scorespro.com/soccer/spain/teams/cadiz-MTc4MDM=/",
+		"osasuna": "https://www.scorespro.com/soccer/spain/teams/osasuna-NDE3OA==/",
+		"valencia": "https://www.scorespro.com/soccer/spain/teams/valencia-MTgwMzc=/",
+		"levante": "https://www.scorespro.com/soccer/spain/teams/levante-MTgwNTI=/",
+		"getafe": "https://www.scorespro.com/soccer/spain/teams/getafe-MTc5MTk=/",
+		"alaves": "https://www.scorespro.com/soccer/spain/teams/deportivo-alaves-MTc4MjQ=/",
+		"valladolid": "https://www.scorespro.com/soccer/spain/teams/valladolid-MTc5NDk=/",
+		"huesca": "https://www.scorespro.com/soccer/spain/teams/huesca-MzA5MzU=/",
+		"elche": "https://www.scorespro.com/soccer/spain/teams/elche-MTc3ODc=/",
+		"elbar": "https://www.scorespro.com/soccer/spain/teams/eibar-MTc3MjE=/"
+	},
+	"test": {
+		"livegame": "https://www.scorespro.com/soccer/china/teams/hebei-zhongji-NDY1ODA=/"
 	}
 }
